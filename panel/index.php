1fe488e6efcec12495ea372a745bac87275cba16
losmok <113143965+losmok@users.noreply.github.com>
2023-04-12 10:15:09 +0200

    Add files via upload

--- panel/index.php
+++ panel/index.php
@@ -11,24 +11,31 @@
     <div class="baner"><h3>Potral Społecznościowy-panel administratora</h3></div>
     <div class="lewy">
         <h4>Użytkownicy</h4>
-        <script>
-            $con = new mysqli('127.0.0.1','root','dane4');
-            $sql = "SELECT id, imie, nazwisko, rok_urodzenia, zdjecie FROM osoby WHERE id BETWEEN 0 AND 30;";
-            $res = $con->query($sql);
-            $rows $res->fetch_all()
-            echo"<ol>";
-            for($i=0,$i<10;$i++) {
-                echo"<li>".$cos[$i][0]." ".cos[$i][1]." ".cos[$i][2]."</li>"
-            }
-        </script>
+        <?php
+           $con = new mysqli("127.0.0.1","root","","dane4");
+            $res = ("SELECT id, imie, nazwisko, rok_urodzenia, zdjecie FROM osoby limit 30");
+            $cos = mysqli_query($con, $res);
+            while($wiersz=mysqli_fetch_array($cos)){
+                $wiek = 2023 - $wiersz[3];
+                echo "$wiersz[0]. $wiersz[1] $wiersz[2], $wiek lat <br>";
+        }
+        ?>
     </div>
     <div class="prawy">
         <h4>Podaj id użytkownika</h4>
         <form action="index.php" method="post">
-            <input type="number" name="poleid">
+            <input type="number" name="id">
             <button>ZOBACZ</button>
         </form>
         <hr>
+        <?php
+        if(isset($_POST["id"])){
+            $res1 = ("SELECT id, imie, nazwisko, rok_urodzenia, opis, zdjecie FROM osoby INNER JOIN hobby ON osoby.id=hobby.id WHERE osoby.id=".$_POST["id"].";");
+            $cos1 = mysqli_query($con, $res1);
+            $wynik = mysqli_fetch_array($cos1);
+        }
+        echo ();
+        ?>
     </div>
     <div class="stopka"><p>Stronę wykonał: Paweł Lewandowski</p></div>
 </body>
